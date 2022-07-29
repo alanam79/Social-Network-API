f640abcf7d0cb603217bee0e813fd8181705f6cc
Alana <halo2382001@yahoo.com>
2022-07-29 17:38:03 -0400

    setting up user model and controller

---
+++ models/User.js
@@ -0,0 +1,52 @@
+const { Schema, model } = require("mongoose");
+// importing function from utils folder for the timestamp
+const dateFormat = require("../utils/dateFormat");
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: "Username is required!",
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: "An email is required!",
+      unique: true,
+      match: [/.+@.+\..+/, "Please enter a valid e-mail address"],
+    },
+    thoughts: [
+      {
+        // an array of ID Values
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        // an array of ID Values
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    // tell the schema it will use virtuals and getters
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    // set id to false because this is a virtual that Mongoose returns (and its not needed here)
+    id: false,
+  }
+);
+
+// get total count of friends - using the virtuals function
+UserSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
